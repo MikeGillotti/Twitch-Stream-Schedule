80ee47aff14b81043403f48be48c33ea9eb913c7
MikeGillotti <38792737+MikeGillotti@users.noreply.github.com>
2023-01-22 03:16:04 -0700

    Dynamic page functionality
    Added the ability for the user to generate schedules based on login name input

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ require('dotenv').config();
 const { Console } = require('console');
 const request = require('request');
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer=require('multer');
+const { resolve } = require('path');
 const app=express();
+const upload=multer();
+
+let streamSchedule = [];
+var accessToken = '';
 
 app.use("/views/css", express.static(__dirname + "/views/css"));
 
   app.use(express.static("views"))
+  
+  app.set("view engine", "ejs")
+
+  app.set('views', './views');
+
+  
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(upload.array());
 
 const getToken = (url, callback) => {
     const options = {
@@ -22,38 +41,48 @@ const getToken = (url, callback) => {
         callback(res);
     });
 };
-
-var accessToken = '';
-const followerList = [];
 getToken(process.env.GET_TOKEN, (res) => {
     accessToken = res.body.access_token;
     return accessToken;
 })
-const getFollow = () => {
-    const followOptions = {
-        url: process.env.GET_FOLLOWING,
+
+
+const getUser = async (x) => {
+    let nameID;
+
+    const userOptions = {
+        url: x,
         method: 'GET',
         headers: {
             'Client-ID': process.env.CLIENT_ID,
             'Authorization': 'Bearer ' + accessToken
         }
     };
-    request.get(followOptions, (err, res, body) => {
-        if (err) {
-            return console.log(err);
+    return new Promise(function (resolve, reject) {
+    
+    request.get(userOptions,  async(err, res, body) =>  {
+        const obj3 =   JSON.parse(body);
+        for(x in obj3.data){
+
+            nameID=  await obj3.data[x].id;
         }
-        const obj = JSON.parse(body);
-        for (let x in obj.data) {
-            followerList[x] = obj.data[x].to_id;
-        }
-        return followerList;
-    });
+
+          
+           resolve(nameID);
+          });
+
+    })
+
 };
 
-const streamSchedule = [];
-const getSchedule = (schedule) => {
-    const scheduleOptions = {
-        url: schedule,
+
+
+const getFollow = async (x) => {
+    let followerList = [];
+
+
+    const followOptions = {
+        url: 'https://api.twitch.tv/helix/users/follows?from_id='+x,
         method: 'GET',
         headers: {
             'Client-ID': process.env.CLIENT_ID,
@@ -61,48 +90,113 @@ const getSchedule = (schedule) => {
         }
     };
 
-    request.get(scheduleOptions, (err, res, body) => {
-        if (err) {
-            console.log(err)
+    return new Promise(function (resolve, reject) {
+
+    request.get(followOptions, async (err, res, body) => {
+        const obj = JSON.parse(body);
+        for (let x in obj.data) {
+            followerList[x] =  obj.data[x].to_id;
         }
-        const obj2 = JSON.parse(body);
+        resolve(followerList);
+    });
+
+});
+
+
+};
+
+
+      streamSchedule=[];
+
+const getSchedule = async (f) => {
+
+    
+    
+    return new Promise(function (resolve, reject) {
+    for (let x in f) {
+        
+    
+
+ const scheduleOptions = {
+        url: process.env.GET_SCHEDULE + "?broadcaster_id=" + f[x],
+        method: 'GET',
+        headers: {
+            'Client-ID': process.env.CLIENT_ID,
+            'Authorization': 'Bearer ' + accessToken
+        }
+    };
+
+
+    request.get(scheduleOptions, async (err, res, body) => {
+        const obj2 = await JSON.parse(body);
         if (obj2.error == null) {
            
             for (let y in obj2.data.segments) {
                 const day= new Date(obj2.data.segments[y].start_time).getDate();
                 const d = new Date(obj2.data.segments[y].start_time);
-                streamSchedule.push([obj2.data.segments[y].start_time, obj2.data.broadcaster_name, day]);
+                streamSchedule.push([obj2.data.segments[y].start_time, obj2.data.broadcaster_name, day])
             }
         }
-        return streamSchedule;
+         resolve(streamSchedule.sort());
     });
-};
-setTimeout(() => {
-    getFollow(() => {
-    });
-}, 500)
-setTimeout(() => {
-    for (let x in followerList) {
-        getSchedule(process.env.GET_SCHEDULE + "?broadcaster_id=" + followerList[x], () => {
-        })
-    }
-}, 1000)
+}
 
-setTimeout(() => {
-    streamSchedule.sort();
+});
+
+
+
     
-}, 1500)
+};
 
 
-app.set("view engine", "ejs")
+
+
+app.get('/favicon.ico', (request,response)=>{
+    return 'your faveicon'
+   })
+
 
 
 app.get('/',  (request, response) => {
 
-    response.render('index', {s: streamSchedule})
+    const name_id=request.query.name;
 
-});
+    nameTwitch=name_id;
 
+    streamSchedule=[];
+
+   
+    followerList = [];
+   
+    
+    const urlUser='https://api.twitch.tv/helix/users?login='+name_id;
+
+     getUser(urlUser)
+     .then((Resolved)=>{
+        getFollow(Resolved)
+        .then((Resolved)=>{
+            getSchedule(Resolved)
+            .then((Resolved)=>{
+               
+               setTimeout(()=>{
+                response.render('index', 
+                {
+                    s: Resolved,
+                    n: name_id
+                     
+                });
+
+                }, 1000)
+            })
+        
+
+        
+        })})
+            
+
+
+
+})
 
 
 app.listen(process.env.PORT || 3000);
